b1305c23f5ea075b49f6a9fa9d40920076448fb4
sabila <baesabilsayang@gmail.com>
2022-03-29 19:40:16 +0700

    membuat layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,122 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(const VokasiApp());
+void main() {
+  debugPaintSizeEnabled = false; // Set to true for visual layout
+  runApp(MyApp());
+}
 
-class VokasiApp extends StatelessWidget {
-  const VokasiApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/vokasi_logo-white.png'),
-              const SizedBox(height: 25),
-              // ignore: prefer_const_constructors
-              Text(
-                'sekolah vokasi',
-                style: GoogleFonts.poppins(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'Unggul, Mandiri & Berkarakter',
-                style: GoogleFonts.poppins(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
+    Widget imageSection = Container(
+      child: Image.asset('assets/sabila.jpg'),
+    );
+
+    Widget titleSection = Container(
+      padding: EdgeInsets.only(top: 16),
+      child: Text(
+        'Strawberry Pavlova',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
         ),
       ),
-      theme: ThemeData(scaffoldBackgroundColor: const Color(0XffEE8301)),
-      debugShowCheckedModeBanner: false,
+    );
+
+    Widget descriptionSection = Container(
+        padding: EdgeInsets.all(16),
+        child: Text(
+          'Pavlova is menrique-based dessert named after the Rusian ballerine, '
+          'Anna Pavlova. Pavloa features a crisp crust and soft, light inside, '
+          'topped with fruit, and whipped cream',
+          textAlign: TextAlign.justify,
+        ));
+
+    Widget rateSection = Row(
+      children: <Widget>[
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star),
+      ],
+    );
+
+    Widget reviewSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        rateSection,
+        Text('170 Reviews'),
+      ],
+    );
+
+    Widget menuSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildMenuSection(Icons.bookmark, "Prep", "25 min"),
+        _buildMenuSection(Icons.timer, "Cook", "1 hour"),
+        _buildMenuSection(Icons.fastfood, "Feeds", "4-6"),
+      ],
+    );
+
+    return MaterialApp(
+      title: 'Material Apps',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Icon(Icons.arrow_back_sharp, color: Colors.black),
+          title: Text(
+            'Profil',
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: <Widget>[Icon(Icons.create_sharp)],
+        ),
+        body: ListView(
+          children: [
+            imageSection,
+            titleSection,
+            descriptionSection,
+            Container(
+              padding: EdgeInsets.only(bottom: 24),
+              child: reviewSection,
+            ),
+            menuSection,
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget _buildTextSection(
+  String text,
+  double textSize,
+  double paddingTop,
+) {
+  return Container(
+    padding: EdgeInsets.only(top: paddingTop),
+    child: Text(
+      text,
+      style: TextStyle(
+        fontSize: textSize,
+      ),
+    ),
+  );
+}
+
+Widget _buildMenuSection(
+  IconData iconData,
+  String title,
+  String timestamp,
+) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(iconData),
+      _buildTextSection(title, 16, 8),
+      _buildTextSection(timestamp, 12, 12),
+    ],
+  );
 }
